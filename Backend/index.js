ad6255ab7213bcd911b69a1da28dd7dc3b778c9e
divykasodariya <kasodariyadivy7@gmail.com>
2025-06-07 23:30:38 +0530

    deployment

--- Backend/index.js
+++ Backend/index.js
@@ -15,7 +15,7 @@ app.use(express.urlencoded({extended:true}));
 app.use(express.json());
 app.use(cookieParser());
 const corsOptions = {
-    origin: "http://localhost:5173",
+    origin: "https://pingy-chat-app.vercel.app/",
     credentials: true,
 }
 app.use(cors(corsOptions));
